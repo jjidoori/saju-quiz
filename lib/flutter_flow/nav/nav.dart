fc6de6cfd0abf30c17de5f9b41780a1b4f2231de
jjidoori <litjhc@gmail.com>
2026-06-26 11:53:16 +0900

    Update nav.dart

--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -108,11 +108,11 @@ GoRouter createRouter(AppStateNotifier appStateNotifier) => GoRouter(
   name: ResultExplanationWidget.routeName,
   path: ResultExplanationWidget.routePath,
   builder: (context, params) => ResultExplanationWidget(
-    questionId: params.getParam('questionId', ParamType.String),
-    isCorrect: params.getParam('isCorrect', ParamType.bool),
-                explanationText: params.getParam(
-  'explanationText',
-  ParamType.String,
+  questionId: params.getParam('questionId', ParamType.String),
+  isCorrect: params.getParam('isCorrect', ParamType.bool),
+  explanationText: params.getParam('explanationText', ParamType.String),
+  questionNumber: params.getParam('questionNumber', ParamType.int),
+  answeredCorrect: params.getParam('answeredCorrect', ParamType.int),
 ),
           ),
         ),
