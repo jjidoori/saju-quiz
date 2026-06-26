b68fecf7f0c00f444ee687391d0959608ffb5cd9
jjidoori <litjhc@gmail.com>
2026-06-26 17:01:01 +0900

    Update welcome_screen_widget.dart

--- lib/pages/welcome_screen/welcome_screen_widget.dart
+++ lib/pages/welcome_screen/welcome_screen_widget.dart
@@ -386,7 +386,7 @@ Container(
   DailySajuChallengeWidget.routeName,
   context.mounted,
   queryParameters: {
-    'category': serializeParam('음양' as String, ParamType.String),
+    'category': '음양',
   },
 );
                                         },
