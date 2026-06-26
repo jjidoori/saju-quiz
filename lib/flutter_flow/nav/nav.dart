8bc8ae3ee6401ce7e71b31e0c3ef520f14d21717
jjidoori <litjhc@gmail.com>
2026-06-26 16:16:38 +0900

    Update nav.dart

--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -88,6 +88,7 @@ GoRouter createRouter(AppStateNotifier appStateNotifier) => GoRouter(
   questionNumber: params.getParam('questionNumber', ParamType.int),
   answeredCorrect: params.getParam('answeredCorrect', ParamType.int),
   questionIds: params.getParam<String>('questionIds', ParamType.String, isList: true),
+  category: params.getParam('category', ParamType.String),
 ),
         ),
         FFRoute(
