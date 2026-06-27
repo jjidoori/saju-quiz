ae6ef4af1882c0b547a9b46305607500e42dadb9
jjidoori <litjhc@gmail.com>
2026-06-27 15:55:16 +0900

    Update app_state.dart

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
 
 class FFAppState extends ChangeNotifier {
   static FFAppState _instance = FFAppState._internal();
@@ -12,8 +13,10 @@ class FFAppState extends ChangeNotifier {
   }
 
   Future initializePersistedState() async {
-    final prefs = await SharedPreferences.getInstance();
-    _completedCategories = prefs.getStringList('completedCategories') ?? [];
+    final saved = html.window.localStorage['completedCategories'];
+    if (saved != null && saved.isNotEmpty) {
+      _completedCategories = saved.split(',');
+    }
   }
 
   void update(VoidCallback callback) {
@@ -23,69 +26,48 @@ class FFAppState extends ChangeNotifier {
 
   int _userLevel = 1;
   int get userLevel => _userLevel;
-  set userLevel(int value) {
-    _userLevel = value;
-  }
+  set userLevel(int value) { _userLevel = value; }
 
   int _totalPoints = 0;
   int get totalPoints => _totalPoints;
-  set totalPoints(int value) {
-    _totalPoints = value;
-  }
+  set totalPoints(int value) { _totalPoints = value; }
 
   int _streakCount = 0;
   int get streakCount => _streakCount;
-  set streakCount(int value) {
-    _streakCount = value;
-  }
+  set streakCount(int value) { _streakCount = value; }
 
   bool _hanjaEnabled = true;
   bool get hanjaEnabled => _hanjaEnabled;
-  set hanjaEnabled(bool value) {
-    _hanjaEnabled = value;
-  }
+  set hanjaEnabled(bool value) { _hanjaEnabled = value; }
 
   bool _soundEnabled = false;
   bool get soundEnabled => _soundEnabled;
-  set soundEnabled(bool value) {
-    _soundEnabled = value;
-  }
+  set soundEnabled(bool value) { _soundEnabled = value; }
 
   String _searchQuery = '';
   String get searchQuery => _searchQuery;
-  set searchQuery(String value) {
-    _searchQuery = value;
-  }
+  set searchQuery(String value) { _searchQuery = value; }
 
   bool _isSearchMode = false;
   bool get isSearchMode => _isSearchMode;
-  set isSearchMode(bool value) {
-    _isSearchMode = value;
-  }
+  set isSearchMode(bool value) { _isSearchMode = value; }
 
   String _dictionaryFilter = 'All';
   String get dictionaryFilter => _dictionaryFilter;
-  set dictionaryFilter(String value) {
-    _dictionaryFilter = value;
-  }
+  set dictionaryFilter(String value) { _dictionaryFilter = value; }
 
   List<String> _todayQuestionIds = [];
   List<String> get todayQuestionIds => _todayQuestionIds;
-  set todayQuestionIds(List<String> value) {
-    _todayQuestionIds = value;
-  }
+  set todayQuestionIds(List<String> value) { _todayQuestionIds = value; }
 
   List<String> _completedCategories = [];
   List<String> get completedCategories => _completedCategories;
-  set completedCategories(List<String> value) {
-    _completedCategories = value;
-  }
+  set completedCategories(List<String> value) { _completedCategories = value; }
 
-  void completeCategory(String category) async {
+  void completeCategory(String category) {
     if (!_completedCategories.contains(category)) {
       _completedCategories = [..._completedCategories, category];
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setStringList('completedCategories', _completedCategories);
+      html.window.localStorage['completedCategories'] = _completedCategories.join(',');
       notifyListeners();
     }
   }
