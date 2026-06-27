b9bbb389a1d30dbed305a2f7c5699184134d699a
jjidoori <litjhc@gmail.com>
2026-06-27 15:25:38 +0900

    Update result_explanation_widget.dart

--- lib/pages/result_explanation/result_explanation_widget.dart
+++ lib/pages/result_explanation/result_explanation_widget.dart
@@ -260,15 +260,18 @@ class _ResultExplanationWidgetState extends State<ResultExplanationWidget> {
                                   context.pushNamed(
                                     QuizResultWidget.routeName,
                                     queryParameters: {
-                                    'category': serializeParam(
-  widget.category ?? '음양',
-  ParamType.String,
-),
-'subCategory': serializeParam(
-  widget.subCategory,
-  ParamType.String,
-),
-}.withoutNulls,
+                                      'category': serializeParam(
+                                        widget.category ?? '음양',
+                                        ParamType.String,
+                                      ),
+                                      'subCategory': serializeParam(
+                                        widget.subCategory,
+                                        ParamType.String,
+                                      ),
+                                      'correctCount': serializeParam(
+                                        correctCount,
+                                        ParamType.int,
+                                      ),
                                     }.withoutNulls,
                                   );
                                 } else {
@@ -288,15 +291,15 @@ class _ResultExplanationWidgetState extends State<ResultExplanationWidget> {
                                         ParamType.String,
                                         isList: true,
                                       ),
-                                  'category': serializeParam(
-  widget.category ?? '음양',
-  ParamType.String,
-),
-'subCategory': serializeParam(
-  widget.subCategory,
-  ParamType.String,
-),
-}.withoutNulls,
+                                      'category': serializeParam(
+                                        widget.category ?? '음양',
+                                        ParamType.String,
+                                      ),
+                                      'subCategory': serializeParam(
+                                        widget.subCategory,
+                                        ParamType.String,
+                                      ),
+                                    }.withoutNulls,
                                   );
                                 }
                               },
