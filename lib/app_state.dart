18b481989eef82c2d84ce2c7101d7c229abd8364
jjidoori <litjhc@gmail.com>
2026-06-29 17:33:41 +0900

    Update app_state.dart

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -83,7 +83,8 @@ bool isCategoryUnlocked(String category) {
     if (idx < 0) return false;
 if (order[idx - 1] == '음양') {
       return _completedCategories.contains('음양_심화');
-if (order[idx - 1] == '오행') {
+    }
+    if (order[idx - 1] == '오행') {
       return _completedCategories.contains('오행_부족');
     }
     return _completedCategories.contains(order[idx - 1]);
