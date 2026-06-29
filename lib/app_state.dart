59a4fc27d7c372277af5102502eb2433362eade5
jjidoori <litjhc@gmail.com>
2026-06-29 14:21:05 +0900

    Update app_state.dart

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -81,9 +81,6 @@ bool isCategoryUnlocked(String category) {
     final idx = order.indexOf(category);
     if (idx == 0) return true;
     if (idx < 0) return false;
-    if (order[idx - 1] == '음양') {
-      return _completedCategories.contains('음양_심화');
-    }
 if (order[idx - 1] == '음양') {
       return _completedCategories.contains('음양_음양심화');
     }
