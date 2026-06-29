e13b6e4e13fd3bb24a9c5e5b2556d09cb4a86f67
jjidoori <litjhc@gmail.com>
2026-06-29 15:39:24 +0900

    Update quiz_result_widget.dart

--- lib/pages/quiz_result/quiz_result_widget.dart
+++ lib/pages/quiz_result/quiz_result_widget.dart
@@ -112,8 +112,8 @@ class _QuizResultWidgetState extends State<QuizResultWidget> {
   String completeKey;
 if (category == '오행') {
     completeKey = (sub != null && sub.isNotEmpty) ? '오행_$sub' : '오행_오행기초';
-  } else if (category == '음양' && sub != null && sub.isNotEmpty) {
-    completeKey = '음양_$sub';
+} else if (category == '음양' && sub != null && sub.isNotEmpty) {
+    completeKey = sub;
   } else {
     completeKey = category;
   }
