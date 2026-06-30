526b95ef39bee3cfb5c50c6a248246b397d032e1
jjidoori <litjhc@gmail.com>
2026-06-30 15:00:31 +0900

    Update daily_saju_challenge_widget.dart

--- lib/pages/daily_saju_challenge/daily_saju_challenge_widget.dart
+++ lib/pages/daily_saju_challenge/daily_saju_challenge_widget.dart
@@ -41,8 +41,9 @@ class _DailySajuChallengeWidgetState extends State<DailySajuChallengeWidget> {
   bool _isLoading = true;
   late int _currentQuestion;
   late int _correctCount;
-  List<String> _generatedIds = [];
+ List<String> _generatedIds = [];
   List<String> _shuffledOptions = [];
+  bool _submitted = false;
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -349,6 +350,8 @@ final newCorrectIndex = opts.indexOf(correctAnswer);
                         highlightColor: Colors.transparent,
                       onTap: () async {
                           if (_model.selectedIndex == null) return;
+                          if (_submitted) return;
+                          _submitted = true;
                           final isCorrect = _model.selectedIndex == _model.correctIndex;
                           final newCorrectCount = _correctCount + (isCorrect ? 1 : 0);
 
