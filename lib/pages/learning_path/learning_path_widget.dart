ec92df62613d7c872b93054104e55e80d996fb21
jjidoori <litjhc@gmail.com>
2026-06-30 14:15:26 +0900

    Update learning_path_widget.dart

--- lib/pages/learning_path/learning_path_widget.dart
+++ lib/pages/learning_path/learning_path_widget.dart
@@ -22,10 +22,7 @@ class _LearningPathWidgetState extends State<LearningPathWidget> {
   late LearningPathModel _model;
   final scaffoldKey = GlobalKey<ScaffoldState>();
 bool _ohangExpanded = false;
-  bool _eumyangExpanded = false;
-
-  @override
-
+bool _eumyangExpanded = false;
 
   final List<Map<String, dynamic>> _eumyangSubcategories = [
 {'subCategory': '음양_일반', 'title': '음양 일반', 'subtitle': '음과 양의 기초 개념과 자연 현상'},
