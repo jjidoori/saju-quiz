4da68fc3825324136126d329d85c8742d0c420f4
jjidoori <litjhc@gmail.com>
2026-08-13 19:24:55 +0900

    Create saju_card_model.dart

---
+++ lib/pages/saju_card/saju_card_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'saju_card_widget.dart' show SajuCardWidget;
+import 'package:flutter/material.dart';
+
+class SajuCardModel extends FlutterFlowModel<SajuCardWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
