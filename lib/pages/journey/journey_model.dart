92b0534427ad73fe77d7d79bba54302cbb3dcec2
jjidoori <litjhc@gmail.com>
2026-08-18 18:30:41 +0900

    Create journey_model.dart

---
+++ lib/pages/journey/journey_model.dart
@@ -0,0 +1,15 @@
+import '/flutter_flow/flutter_flow_util.dart';
+
+class JourneyModel extends FlutterFlowModel {
+  /// Initialization and disposal methods.
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
