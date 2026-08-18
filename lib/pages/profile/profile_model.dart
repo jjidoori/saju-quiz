8f5849fa98502435495430a1a3d16752ccb3ba3f
jjidoori <litjhc@gmail.com>
2026-08-18 20:34:52 +0900

    Create profile_model.dart

---
+++ lib/pages/profile/profile_model.dart
@@ -0,0 +1,23 @@
+// Export pages
+export '/pages/auth_screen/auth_screen_widget.dart' show AuthScreenWidget;
+export '/pages/saju_card/saju_card_widget.dart' show SajuCardWidget;
+export '/pages/daily_saju_challenge/daily_saju_challenge_widget.dart'
+    show DailySajuChallengeWidget;
+export '/pages/element_dictionary/element_dictionary_widget.dart'
+    show ElementDictionaryWidget;
+export '/pages/learning_path/learning_path_widget.dart' show LearningPathWidget;
+export '/pages/quiz_interface/quiz_interface_widget.dart'
+    show QuizInterfaceWidget;
+export '/pages/result_explanation/result_explanation_widget.dart'
+    show ResultExplanationWidget;
+export '/pages/settings/settings_widget.dart' show SettingsWidget;
+export '/pages/user_profile_progress/user_profile_progress_widget.dart'
+    show UserProfileProgressWidget;
+export '/pages/welcome_screen/welcome_screen_widget.dart'
+    show WelcomeScreenWidget;
+export '/pages/quiz_result/quiz_result_widget.dart'
+    show QuizResultWidget;
+export '/pages/journey/journey_widget.dart' show JourneyWidget;
+export '/pages/practice/practice_widget.dart' show PracticeWidget;
+export '/pages/mastery/mastery_widget.dart' show MasteryWidget;
+export '/pages/profile/profile_widget.dart' show ProfileWidget;
